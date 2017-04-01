18f25535035ffb88e02b40a7206ffa361a172c59
nicole <nicolew@shapeways.com>
2017-03-31 22:35:52 -0400

    adding flash messages to user form

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, url_for, \
+    redirect
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from config import BaseConfig
@@ -37,6 +38,14 @@ def add_user():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
 
+    if not first_name:
+        flash('First Name not specified')
+        return redirect(url_for('get_one_user'))
+
+    if not last_name:
+        flash('Last Name not specified')
+        return redirect(url_for('get_one_user', first_name=first_name))
+
     result = user_db.insert_one({
         'first_name': first_name,
         'last_name': last_name
@@ -51,19 +60,21 @@ def add_user():
     return jsonify({'result': output})
 
 
+@app.route('/user/', methods=['GET'])
 @app.route('/user/<first_name>', methods=['GET'])
-def get_one_user(first_name):
+def get_one_user(first_name=None):
     user_db = client.test.user
 
-    user = user_db.find_one({'first_name': first_name})
-    if user:
-        output = {
-            'first_name': user['first_name'],
-            'last_name': user['last_name']
-        }
-        return jsonify({'result': output})
-    else:
-        return render_template('user.html', first_name=first_name)
+    if first_name is not None:
+        user = user_db.find_one({'first_name': first_name})
+        if user:
+            output = {
+                'first_name': user['first_name'],
+                'last_name': user['last_name']
+            }
+            return jsonify({'result': output})
+
+    return render_template('user.html', first_name=first_name)
 
 
 if __name__ == '__main__':
