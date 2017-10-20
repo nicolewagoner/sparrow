0264a99dbd4d5a5108176659d8378fc36e01d049
nicole <nicolew@shapeways.com>
2017-10-20 18:15:01 -0400

    adding index for app

--- web/app.py
+++ web/app.py
@@ -13,8 +13,25 @@ sentry = Sentry(
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!!'
+def index():
+    return render_template('index.html')
+
+
+# @app.route('/user/', methods=['GET'])
+# @app.route('/user/<first_name>', methods=['GET'])
+# def get_one_user(first_name=None):
+#     user_db = client.test.user
+#
+#     if first_name is not None:
+#         user = user_db.find_one({'first_name': first_name})
+#         if user:
+#             output = {
+#                 'first_name': user['first_name'],
+#                 'last_name': user['last_name']
+#             }
+#             return jsonify({'result': output})
+#
+#     return render_template('user.html', first_name=first_name)
 
 
 @app.errorhandler(BadRequest)
