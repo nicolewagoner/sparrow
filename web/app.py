30f6de3af33cf1a53ec95cf5c04b9c1f377cee09
nicole <nicolew@shapeways.com>
2017-10-21 17:36:46 -0400

    adding basic auth flow for spotipy

--- web/app.py
+++ web/app.py
@@ -6,6 +6,8 @@ from werkzeug.exceptions import BadRequest
 import spotipy
 import spotipy.oauth2 as oauth2
 import spotipy.util as util
+# from random import choice
+# from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
@@ -61,21 +63,50 @@ def test_authorize_spotify():
     else:
         return "Can't get token for user", username
 
-# @app.route('/user/', methods=['GET'])
-# @app.route('/user/<first_name>', methods=['GET'])
-# def get_one_user(first_name=None):
-#     user_db = client.test.user
-#
-#     if first_name is not None:
-#         user = user_db.find_one({'first_name': first_name})
-#         if user:
-#             output = {
-#                 'first_name': user['first_name'],
-#                 'last_name': user['last_name']
-#             }
-#             return jsonify({'result': output})
-#
-#     return render_template('user.html', first_name=first_name)
+
+@app.route('/authorize-spotify', methods=['POST', 'GET'])
+def authorize_spotify():
+    # TODO implement location checks
+    # location = request.form['location']
+
+    # TODO implement state & show_dialog
+    # https://github.com/plamere/spotipy/issues/211
+    # stateKey = 'spotify_auth_state'
+    # state = ''.join(choice(ascii_uppercase) for i in range(16))
+    # res.cookie(stateKey, state)
+
+    oauth = oauth2.SpotifyOAuth(
+        client_id=BaseConfig.SPOTIFY_CLIENT_ID,
+        client_secret=BaseConfig.SPOTIFY_CLIENT_SECRET,
+        redirect_uri=BaseConfig.SPOTIFY_REDIRECT_URL,
+        scope='user-library-read')
+
+    return redirect(oauth.get_authorize_url())
+
+
+@app.route('/events')
+def get_all_events():
+    error = request.args.get('error')
+    if error:
+        return error
+
+    auth_code = request.args.get('code')
+    if not auth_code:
+        return "Something went wrong"
+
+    oauth = oauth2.SpotifyOAuth(
+        client_id=BaseConfig.SPOTIFY_CLIENT_ID,
+        client_secret=BaseConfig.SPOTIFY_CLIENT_SECRET,
+        redirect_uri=BaseConfig.SPOTIFY_REDIRECT_URL,
+        scope='user-library-read')
+    token_info = oauth.get_access_token(auth_code)
+    if not token_info:
+        return "Couldn't get access token"
+
+    access_token = token_info['access_token']
+    sp = spotipy.Spotify(auth=access_token)
+    results = sp.current_user_followed_artists()
+    return jsonify({'result': results})
 
 
 @app.errorhandler(BadRequest)
