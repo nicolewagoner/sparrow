3d58b60845bf1da1795f21b1998eed24b58873c8
nicole <nicolew@shapeways.com>
2017-10-21 14:34:49 -0400

    adding spotipy and some test usage

--- web/app.py
+++ web/app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request, jsonify, flash, url_for, \
 from config import BaseConfig
 from raven.contrib.flask import Sentry
 from werkzeug.exceptions import BadRequest
+import spotipy
+import spotipy.oauth2 as oauth2
+import spotipy.util as util
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
@@ -16,6 +19,47 @@ sentry = Sentry(
 def index():
     return render_template('index.html')
 
+
+@app.route('/test')
+def test():
+    credentials = oauth2.SpotifyClientCredentials(
+        client_id=BaseConfig.SPOTIFY_CLIENT_ID,
+        client_secret=BaseConfig.SPOTIFY_CLIENT_SECRET)
+
+    token = credentials.get_access_token()
+    sp = spotipy.Spotify(auth=token)
+
+    output = []
+    results = sp.search(q='weezer', limit=5)
+    for i, t in enumerate(results['tracks']['items']):
+        output.append({
+            'i': i,
+            'name': t['name']
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/test-authorize-spotify')
+def test_authorize_spotify():
+    scope = 'user-library-read'
+    username = "rattlenhumn"
+    credentials = oauth2.SpotifyClientCredentials(
+        client_id=BaseConfig.SPOTIFY_CLIENT_ID,
+        client_secret=BaseConfig.SPOTIFY_CLIENT_SECRET)
+
+    token = util.prompt_for_user_token(
+        username,
+        scope,
+        client_id=BaseConfig.SPOTIFY_CLIENT_ID,
+        client_secret=BaseConfig.SPOTIFY_CLIENT_SECRET,
+        redirect_uri=BaseConfig.SPOTIFY_REDIRECT_URL)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_saved_tracks()
+        return jsonify({'result': results})
+    else:
+        return "Can't get token for user", username
 
 # @app.route('/user/', methods=['GET'])
 # @app.route('/user/<first_name>', methods=['GET'])
--- web/config.py
+++ web/config.py
@@ -4,3 +4,7 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SENTRY_DSN = os.environ['SENTRY_DSN']
+
+    SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+    SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+    SPOTIFY_REDIRECT_URL = os.environ['SPOTIFY_REDIRECT_URL']
